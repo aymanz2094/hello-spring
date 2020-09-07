94772dd2f60990fdef0ecb25257980f984e0343c
aymanz2094 <aymanz2094@gmail.com>
2020-09-07 14:33:19 -0500

    learned using templates

---
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -0,0 +1,60 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+
+public class HelloController {
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello, Spring!";
+//    }
+//    @GetMapping("goodbye")
+//    @ResponseBody
+//    public String goodbye() {
+//        return "Goodbye, Spring!";
+//    }
+
+//    Handles request of the form /hello?coder=LaunchCode
+//    @GetMapping("hello")
+//    @PostMapping("hello")
+
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+
+    public String helloWithQueryParam(@RequestParam String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
+    }
+
+//    handles requests of the form /hello/LaunchCode
+    @GetMapping("hello/{name}")
+
+    public String helloWithPathParam(@PathVariable String name, Model model){
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
+    }
+
+    @GetMapping("form")
+    public String helloForm() {
+        return "form";
+
+    }
+@GetMapping("hello-names")
+    public String helloName(Model model) {
+        List<String> names= new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Java");
+        names.add("JavaScript");
+        model.addAttribute("names", names);
+        return "hello-list";
+    }
+
+}
